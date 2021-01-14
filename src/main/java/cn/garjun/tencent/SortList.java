f87960d8f440b074d831cd8ee35912ad67fc6d02
Administrator <929138066@qq.com>
2021-01-14 22:19:15 +0800

    try

--- src/main/java/cn/garjun/tencent/SortList.java
+++ src/main/java/cn/garjun/tencent/SortList.java
@@ -7,16 +7,20 @@ package cn.garjun.tencent;
  * @email :jaron.liang@jodoinc.com
  */
 public class SortList {
-  public ListNode sortList(ListNode head) {
+  public static ListNode sortList(ListNode head) {
+    if(head == null){
+      return null;
+    }
     int length = 0;
     ListNode temp = head;
     while (temp != null) {
       length++;
       temp = temp.next;
     }
+    return partition(head,0,length-1);
   }
 
-  public ListNode partition(ListNode head, int start, int end) {
+  public static ListNode partition(ListNode head, int start, int end) {
     if (head.next == null) {
       return head;
     }
@@ -28,14 +32,70 @@ public class SortList {
       }
       return head;
     }
-    int middle = (start + end) / 2;
+    int middle = (start + end) / 2 ;
     ListNode nextHead = head;
     int current = start;
-    while (current < middle) {
+    while (current < middle + 1) {
       nextHead = nextHead.next;
       current++;
     }
     ListNode first = partition(head, start, middle);
     ListNode second = partition(nextHead, middle + 1, end);
+    return merge(first,second);
+  }
+  public ListNode merge(ListNode first,ListNode second){
+    ListNode head ;
+    if(first.val < second.val){
+      head = first;
+      first = first.next;
+    }else{
+      head = second;
+      second = second.next;
+    }
+    ListNode result = head;
+    while (first != null && second != null){
+      if(first.val < second.val){
+        head.next = first;
+        first = first.next;
+        head = head.next;
+      }else {
+        head.next = second;
+        second = second.next;
+        head = head.next;
+      }
+    }
+    while (first != null){
+      head.next = first;
+      first = first.next;
+      head = head.next;
+    }
+    while (second != null){
+      head.next = second;
+      second = second.next;
+      head = head.next;
+    }
+    return result;
+
+  }
+
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {4,2,1,3};
+    ListNode listNode = buildList(nums);
+    listNode = sortList(listNode);
+    while (listNode != null) {
+      System.out.println(listNode.val);
+      listNode = listNode.next;
+    }
+  }
+
+  public static ListNode buildList(int[] nums) {
+    ListNode listNode = new ListNode(nums[0]);
+    ListNode head = listNode;
+    for (int i = 1; i < nums.length; i++) {
+      listNode.next = new ListNode(nums[i]);
+      listNode = listNode.next;
+    }
+    return head;
   }
 }
